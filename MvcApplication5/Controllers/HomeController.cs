3a968598ff8a142e3803da9b7cc0da28dd5760f6
seojung <youeseojung@gmail.com>
2016-08-04 11:26:08 +0900

    first

---
+++ MvcApplication5/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private tobyEntities db = new tobyEntities();
+
+        public ActionResult shop()
+        {
+            return RedirectToAction("Index", "Shop");
+        }
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication5/Views/Home/Index.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<IEnumerable<MvcApplication5.account>>" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <meta name="viewport" content="width=device-width" />
+    <title>Index</title>
+</head>
+<body>
+  인덱스 페이지
+</body>
+</html>
