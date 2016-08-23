b903075474d2428a46c5168a3b5bda4b5cc73139
sy <ksang91@naver.com>
2016-08-23 11:10:09 +0900

    dll modify

--- MvcApplication5/Global.asax.cs
+++ MvcApplication5/Global.asax.cs
@@ -18,7 +18,7 @@ namespace MvcApplication5
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+          //  WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
